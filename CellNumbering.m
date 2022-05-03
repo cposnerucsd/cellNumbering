8cfedc476781cdbaa5d2de44230903e3d9893a59
cposnerucsd <99054573+cposnerucsd@users.noreply.github.com>
2022-05-03 16:28:57 -0700

    Add files via upload
    original cell numbering. increases the cell counter if timeInd resets to one on next iteration.

---
+++ CellNumbering.m
@@ -0,0 +1,23 @@
+function[cellNoTbl] = CellNumbering (allTbl)
+%add a variable cell No. to allTbl initialized at 0
+cellNoTbl = allTbl;
+cellNoTbl.cellNo = zeros(size(allTbl,1),1);
+
+cellCounter = 1; %variable to increase when counting cells
+
+%add cell numbers to the cellNo variable col of table
+for i = 1: size(cellNoTbl, 1) %loop through each row  
+   cellNoTbl.cellNo(i) = cellCounter;
+   %increment cell Counter if timeInd resets to one on next i
+   if i == size(cellNoTbl,1)
+       break
+   end
+   if cellNoTbl.timeInd(i+1) == 1
+       cellCounter = cellCounter +1;
+   end
+end
+
+[~,folder_name] = fileparts(pwd); %gets folder name
+writetable(cellNoTbl,[folder_name,'AllTblwcellNos','.xlsx']); %saves table
+
+end
